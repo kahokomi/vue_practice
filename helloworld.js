3f326c824c1934532bcea033be4a7e8feb5e9657
kahokomi <knk61721@gmail.com>
2021-03-20 22:30:49 +0900

    vue練習

--- helloworld.js
+++ helloworld.js
@@ -1,74 +1,80 @@
 // $function() {
 //   $('#top-btn').click(function(){
-//     $('html,body').animate({ 
-//       'scrollTop': 0 
+//     $('html,body').animate({
+//       'scrollTop': 0
 //     }, 'slow');
 //   });
 // };
 
 var app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    message: 'Hello Vue!'
-  }
-})
+    message: "Hello Vue!",
+  },
+});
 
 var app2 = new Vue({
-  el: '#app-2',
+  el: "#app-2",
   data: {
-    message: 'あなたのページ' + new Date().toLocaleDateString()
-  }
-})
+    message: "あなたのページ" + new Date().toLocaleDateString(),
+  },
+});
 
 const app3 = new Vue({
-  el: '#app-3',
+  el: "#app-3",
   data: {
-    seen: true
-  }
-})
+    seen: true,
+  },
+});
 
 var app4 = new Vue({
-  el: '#app-4',
+  el: "#app-4",
   data: {
     todos: [
-      {text: 'Learn JavaScript'},
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
-  }
-})
+      { text: "Learn JavaScript" },
+      { text: "Learn Vue" },
+      { text: "Build something awesome" },
+    ],
+  },
+});
 
 const app5 = new Vue({
-  el: '#app-5',
+  el: "#app-5",
   data: {
-    message: 'Hello Vue.js!'
+    message: "Hello Vue.js!",
   },
   methods: {
     reverseMessage: () => {
-      app5.message = app5.message.split('').reverse().join('')
-    }
-  }
-})
+      app5.message = app5.message.split("").reverse().join("");
+    },
+  },
+});
 
-const app6 = new Vue ({
-  el: '#app-6',
+const app6 = new Vue({
+  el: "#app-6",
   data: {
-    mess: 'Hello Vue!'
-  }
-})
+    mess: "Hello Vue!",
+  },
+});
 
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>This is a todo</li>'
-})
+Vue.component("todo-item", {
+  props: ["todo"],
+  template: "<li>This is a todo</li>",
+});
 
 var app = new Vue({
-  el: '#app-7',
+  el: "#app-7",
   data: {
     groceryList: [
-      { id: 0, text: 'Vegetables' },
-      { id: 1, text: 'Cheese' },
-      { id: 2, text: 'Whatever else humans are supposed to eat'}
-    ]
-  }
-})
+      { id: 0, text: "Vegetables" },
+      { id: 1, text: "Cheese" },
+      { id: 2, text: "Whatever else humans are supposed to eat" },
+    ],
+  },
+});
+
+var data = { a: 1 };
+
+var vm = new Vue({
+  data: data,
+});
